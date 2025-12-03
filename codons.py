9ad4abb8bd74536fc8faa20d1b2672e3bac73fea
rawadkh0 <rawadkh0@gmail.com>
2025-12-03 14:01:27 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,15 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+    codon2aa = {}
+    # Use the provided file_path argument instead of hardcoding
+    file_text = open(file_path,"r")
+    rows = file_text.readlines()
+    file_text.close()
 
-
+    for r in rows:
+      parts = r.strip().split('\t')
+      if len(parts) >= 3:
+       key = parts[0]
+       value = parts[2]
+       codon2aa[key] = value
+    # The function needs to return the dictionary, not just print it.
+    return codon2aa
